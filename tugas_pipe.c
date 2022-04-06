d7e3d2dcd3320f2dbc5562a355cae10a70905c28
Raihan "VieR" Fitrah <56926122+vier15@users.noreply.github.com>
2022-04-07 00:20:07 +0700

    Tugas Pipe v1.0

---
+++ tugas_pipe.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>     /* exit() */
+#include <unistd.h>     /* fork() and getpid() */
+#include <stdio.h>      /* printf() */
+#include <string.h>
+#include <time.h>
+#define MSGSIZE 2
+
+int main(int argc, char **argv) {
+        srand(time(NULL));
+        char inbuf[MSGSIZE]; //buffer
+        int fd[2]; //pipe
+
+        //create pipe
+        if (pipe(fd) < 0) {
+            exit(1); //error
+        }
+        printf("mulai \n");
+        int n, m; 
+        printf("Masukan banyaknya proses 1 yang diinginkan : ");
+        scanf("%d", &n);
+        printf("Masukan banyaknya proses 2 yang diinginkan : ");
+        scanf("%d", &m);
+        int pid;
+            switch (pid = fork()) {
+            case 0:         /* fork returns 0 ke Kelompok Proses 1 */
+                   for(int i = 0; i < n; i++){
+                       printf("Kelompok Proses 1\n");
+                    // tulis data ke pipe
+                    int arrIntChild[MSGSIZE] = {rand() % 100,rand() % 100};
+                    for(int j = 0 ; j < 2 ;j++){
+                        printf("nilai ke %d = %d\n", i+1, arrIntChild[j]);
+                    }
+                    /* tutup bagian input dari pipe */
+                    close(fd[0]);
+                    // tulis ke pipe
+                    write(fd[1], arrIntChild, sizeof(arrIntChild));
+                   } 
+                    break;
+            default:        /* fork returns pid Kelompok Proses 2 */
+                    for(int i = 0; i < m; i++){
+                        printf("Kelompok Proses 2\n");
+                    int arrIntParent[MSGSIZE];
+                    /* tutup bagian output dari pipe */
+                    close(fd[1]);
+                    // baca yang ditulis child dari pipe
+                    read(fd[0], arrIntParent, sizeof(arrIntParent));
+                    int total = 0;
+                    for (int j = 0; j<MSGSIZE; j++) {
+                        total += arrIntParent[i];
+                    }
+                    printf("Total = %d\n", total);
+                    }
+                    
+                    break;
+    
+            case -1:        /* error */
+                    perror("fork");
+                    exit(1);
+            }
+
+        exit(0);
+}
