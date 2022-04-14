5358d63b659433331a7a41e9e0e500b2bd871460
Raihan "VieR" Fitrah <56926122+vier15@users.noreply.github.com>
2022-04-14 09:32:45 +0700

    Tugas Pipe v2

--- tugas_pipe.c
+++ tugas_pipe.c
@@ -3,62 +3,71 @@
 #include <stdio.h>      /* printf() */
 #include <string.h>
 #include <time.h>
-#define MSGSIZE 2
+#define MSGSIZE 5
+
 
 int main(int argc, char **argv) {
-        srand(time(NULL));
         char inbuf[MSGSIZE]; //buffer
         int fd[2]; //pipe
+  			int fdCount[2];
 
         //create pipe
         if (pipe(fd) < 0) {
             exit(1); //error
         }
-        printf("mulai \n");
-        int n, m; 
-        printf("Masukan banyaknya proses 1 yang diinginkan : ");
-        scanf("%d", &n);
-        printf("Masukan banyaknya proses 2 yang diinginkan : ");
-        scanf("%d", &m);
+  			if (pipe(fdCount) < 0) {
+            exit(1); //error
+        }
+        int arrIntProducer[MSGSIZE];
+        int pidProducer, pidConsumer;
         int pid;
-            switch (pid = fork()) {
-            case 0:         /* fork returns 0 ke Kelompok Proses 1 */
-                   for(int i = 0; i < n; i++){
-                       printf("Kelompok Proses 1\n");
-                    // tulis data ke pipe
-                    int arrIntChild[MSGSIZE] = {rand() % 100,rand() % 100};
-                    for(int j = 0 ; j < 2 ;j++){
-                        printf("nilai ke %d = %d\n", j+1, arrIntChild[j]);
-                    }
-                    /* tutup bagian input dari pipe */
-                    close(fd[0]);
-                    // tulis ke pipe
-                    write(fd[1], arrIntChild, sizeof(arrIntChild));
-                   } 
-                    break;
-            default:        /* fork returns pid Kelompok Proses 2 */
-                    for(int i = 0; i < m; i++){
-                        printf("Kelompok Proses 2\n");
-                    int arrIntParent[MSGSIZE];
-                    /* tutup bagian output dari pipe */
-                    close(fd[1]);
-                    // baca yang ditulis child dari pipe
-                    read(fd[0], arrIntParent, sizeof(arrIntParent));
-                    int total = 0;
-                    if(i < n){
-                        for (int j = 0; j<MSGSIZE; j++) {
-                            total += arrIntParent[j];
-                        }
-                    }
-                    printf("Total = %d\n", total);
-                    }
-                    
-                    break;
-    
-            case -1:        /* error */
-                    perror("fork");
-                    exit(1);
-            }
-
+  			int x;
+  			int total = 0;
+  			int grandTotal;
+  			
+  			switch (pid = fork()) {
+        case 0:         /* fork returns 0 ke proses anak */
+                printf("Proses anak \n");
+								int count = 0;
+                for(int i = 0; i < 3 ; i++){
+                  		fork();
+                  		srand(getpid());
+                      x = rand() % 10;
+                      arrIntProducer[count] = x;
+                  		printf("Producer (%d) membuat nilai %d di buffer %d\n", getpid(), arrIntProducer[count], count);
+                      count++;
+                }
+                /* tutup bagian input dari pipe */
+                close(fd[0]);
+            		close(fdCount[0]);
+                // tulis ke pipe
+                write(fd[1], arrIntProducer, sizeof(arrIntProducer));
+            		write(fdCount[1], &count, sizeof(int));
+                break;
+        default:        /* fork returns pid ke proses ortu */
+                printf("Proses ortu\n");
+                /* tutup bagian output dari pipe */
+                close(fd[1]);
+            		close(fdCount[1]);
+                // baca yang ditulis child dari pipe
+                read(fd[0], arrIntProducer,  sizeof(arrIntProducer)); //buffer terisi
+            		read(fdCount[0], &count,  sizeof(int));
+                
+							int grandTotal = 0;
+            	printf("nilai = %d\n", count);
+               	for(int i = 0; i < 2 ; i++){
+                  		fork();
+                      total =  arrIntProducer[count-1];
+                  		printf("Consumer (%d) mengambil nilai %d di buffer %d\n", getpid(),total, count-1);
+                  		grandTotal += total;
+                  		count--;
+                }
+								
+                break;
+        case -1:        /* error */
+                perror("fork");
+                exit(1);
+        }
+				printf("total = %d\n", grandTotal);
         exit(0);
 }
